d4bc38c4ce62390431d5da6ef80ca7d52147f058
srilakshmi <dirisalasrilakshmi@gmail.com>
2023-02-24 15:34:56 +0530

    reacttask3 is added

---
+++ src/components/Dummy/Dummy.js
@@ -0,0 +1,10 @@
+import styles from "./index.module.css"
+const Dummy =()=> {
+    return (
+        <div className={`${styles.continer} cwergwer`}>
+            <h1>About Page</h1>
+        </div>
+    );
+}
+
+export default Dummy
